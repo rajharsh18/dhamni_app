f7f34d4b080a871da60814e86fe116ecbdd7b523
rajharsh18 <rajharsh564@gmail.com>
2023-04-03 19:59:29 +0530

    Added common widgets

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:dhamni/firebase_options.dart';
+import 'package:dhamni/src/repository/authentication_repository/authentication_repository.dart';
+import 'package:dhamni/src/utils/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((value) => Get.put(AuthenticationRepository()));
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      defaultTransition: Transition.leftToRightWithFade,
+      transitionDuration: const Duration(milliseconds: 500),
+      home: CircularProgressIndicator(),
+    );
+  }
+}
