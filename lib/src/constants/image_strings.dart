908dbaf6274dc5cf7bdc5f753e9edc815c1fab67
rajharsh18 <rajharsh564@gmail.com>
2023-04-03 20:00:26 +0530

    Added constants

---
+++ lib/src/constants/image_strings.dart
@@ -0,0 +1,13 @@
+/* -- App Image Strings -- */
+
+// -- Welcome Screen Images
+// -- also used in Login & SignUp
+const String tWelcomeScreenImage =
+    "assets/images/welcome_images/donate_blood.png";
+
+// -- Login & SignUp Images
+const String tGoogleLogoImage = "assets/logo/google.png";
+
+// -- Forget Password Images
+const String tForgetPasswordImage = "assets/images/forget-password.png";
+const String tUserProfileImage = "assets/images/dashboard/profile_image.png";
---
+++ lib/src/constants/text_strings.dart
@@ -0,0 +1,69 @@
+/* -- App Text Strings -- */
+
+// -- GLOBAL Texts
+const String tNext = "Next";
+const String tLogin = "Login";
+const String tEmail = "E-Mail";
+const String tSignup = "Signup";
+const String tPhoneNo = "Phone No";
+const String tPassword = "Password";
+const String tFullName = "Full Name";
+const String tForgetPassword = "Forget Password";
+const String tSignInWithGoogle = "Sign-In with Google";
+
+// -- Welcome Screen Text
+const String tAppName = "Dhamni";
+const String tWelcomeTitle = "Dhamni";
+const String tWelcomeSubTitle = "Connecting Blood";
+
+// -- Login Screen Text
+const String tLoginTitle = "Welcome Back,";
+const String tLoginSubTitle = "Make it work, make it right, make it fast.";
+const String tRememberMe = "Remember Me?";
+const String tDontHaveAnAccount = "Don't have an Account? ";
+
+// -- Sign Up Screen Text
+const String tSignUpTitle = "Get On Board!";
+const String tSignUpSubTitle = "Create your profile to start your Journey.";
+const String tAlreadyHaveAnAccount = "Already have an Account? ";
+
+// -- Forget Password Text
+const String tForgetPasswordTitle = "Make Selection!";
+const String tForgetPasswordSubTitle =
+    "Select one of the options given below to reset your password.";
+const String tResetViaEMail = "Reset via Mail Verification";
+const String tResetViaPhone = "Reset via Phone Verification";
+
+// -- Forget Password Via Phone - Text
+const String tForgetPhoneSubTitle =
+    "Enter your registered Phone No to receive OTP";
+
+// -- Forget Password Via E-Mail - Text
+const String tForgetMailSubTitle =
+    "Enter your registered E-Mail to receive OTP";
+
+// -- OTP Screen - Text
+const String tOtpTitle = "CO\nDE";
+const String tOtpSubTitle = "Verirification";
+const String tOtpMessage = "Enter the verification code sent at ";
+
+// -- Dashboard - Text
+const String tDashboardTitle = "Hey, Coding with T";
+const String tDashboardSubTitle = "Hey, Coding with T";
+const String tDashboardSearch = "Search...";
+
+// --Profile Screen - Text
+
+const String tProfile = "Profile";
+const String tEditProfile = "Edit Profile";
+const String tLogoutDialogHeading = "Logout";
+const String tProfileHeading = "Coding With Harsh";
+const String tProfileSubHeading = "harsh@gmail.com";
+// const String tMenu5 = tLogout;
+const String tMenu4 = "User MAnagement";
+const String tMenu3 = "Billing Details";
+const String tMenu2 = "Informations";
+const String tMenu1 = "Settings";
+const String tDelete = "Delete";
+const String tJoined = "Joined";
+const String tJoinedAt = "31 October 2022";
